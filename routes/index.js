8a2e215db514d1d88d7307993e5b2e5eee4ebf08
Jeffrey El boustani <jelboustani95@gmail.com>
2018-03-04 23:03:36 +1100

    Moved validation and filter function to separate functions to clear up code in route

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.post('/', function(req, res, next) {
 	const payload = req.body.payload;
 
 	if (Array.isArray(payload) && payload.every(isPropertyValid)) {
-		const propertiesArr = payload.filter((property) => {
-			return (property.workflow === "completed" && property.type === "htv");
-		}).map((property) => {
-			const address = concatAddress(property.address);
-			return {
-				concataddress: address,
-				workflow: property.workflow,
-				type: property.type
+		const propertiesArr = payload.filter(isCompleteAndHtv)
+		.map((property) => buildAddress(property.address))
+		.map((fullAddress) => {
+		  	return {
+			    concataddress: fullAddress,
+			    workflow: "completed",
+			    type: "htv"
 			};
-		});
+		})
 
 		res.status(200).json({
 			response: propertiesArr
@@ -29,18 +27,23 @@ router.post('/', function(req, res, next) {
 
 module.exports = router;
 
-function isPropertyValid(property) {
-	return (property.workflow && property.type && property.address);
+// Ensures the property has all the required fields
+function isPropertyValid({workflow, type, address}) {
+	return (workflow && type && address && isAddressValid(address));
 }
 
-function concatAddress({unitNumber, buildingNumber, street, suburb, state, postcode}) {
-	let address = "";
-	address += (unitNumber != undefined ? unitNumber + ' ' : '');
-	address += buildingNumber + ' ';
-	address += street + ' ';
-	address += suburb + ' ';
-	address += state + ' ';
-	address += postcode + ' ';
+// Ensures property has the required address fields
+function isAddressValid({buildingNumber, street, suburb, state, postcode}) {
+	return (buildingNumber && street && suburb && state && postcode);
+}
+
+function isCompleteAndHtv(property) {
+	return (property.workflow === "completed" && property.type === "htv");
+}
+
+function buildAddress({unitNumber, buildingNumber, street, suburb, state, postcode}) {
+	const address = `${(unitNumber != undefined ? unitNumber + ' ' : '')}` +
+		`${buildingNumber} ${street} ${suburb} ${state} ${postcode}`;
 
 	return address;
 }
