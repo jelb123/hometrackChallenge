0447fac5ea5fb9273daf12cf78ccc232f51d8ae0
Jeffrey El boustani <jelboustani95@gmail.com>
2018-03-04 21:52:58 +1100

    Add error handling to ensure payload is an Array and required fields exist in each property

--- hometrackApp/routes/index.js
+++ hometrackApp/routes/index.js
@@ -5,23 +5,33 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
 	const payload = req.body.payload;
 
-	const propertiesArr = payload.filter((property) => {
-		return (property.workflow === "completed" && property.type === "htv");
-	}).map((property) => {
-		const address = concatAddress(property.address);
-		return {
-			concataddress: address,
-			workflow: property.workflow,
-			type: property.type
-		};
-	});
+	if (Array.isArray(payload) && payload.every(isPropertyValid)) {
+		const propertiesArr = payload.filter((property) => {
+			return (property.workflow === "completed" && property.type === "htv");
+		}).map((property) => {
+			const address = concatAddress(property.address);
+			return {
+				concataddress: address,
+				workflow: property.workflow,
+				type: property.type
+			};
+		});
 
-	res.status(200).json({
-		response: propertiesArr
-	});
+		res.status(200).json({
+			response: propertiesArr
+		});
+	} else {
+		res.status(400).json({
+			"error": "Could not decode request: JSON parsing failed"
+		});
+	}
 });
 
 module.exports = router;
+
+function isPropertyValid(property) {
+	return (property.workflow && property.type && property.address);
+}
 
 function concatAddress({unitNumber, buildingNumber, street, suburb, state, postcode}) {
 	let address = "";
