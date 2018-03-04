915888213403b2dba698393fe0ccc272d93982f5
Jeffrey El boustani <jelboustani95@gmail.com>
2018-03-04 21:29:48 +1100

    Add property filter and concatAddress function

--- hometrackApp/routes/index.js
+++ hometrackApp/routes/index.js
@@ -2,8 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res, next) {
+	const payload = req.body.payload;
+
+	const propertiesArr = payload.filter((property) => {
+		return (property.workflow === "completed" && property.type === "htv");
+	}).map((property) => {
+		const address = concatAddress(property.address);
+		return {
+			concataddress: address,
+			workflow: property.workflow,
+			type: property.type
+		};
+	});
+
+	res.status(200).json({
+		response: propertiesArr
+	});
 });
 
 module.exports = router;
+
+function concatAddress({unitNumber, buildingNumber, street, suburb, state, postcode}) {
+	let address = "";
+	address += (unitNumber != undefined ? unitNumber + ' ' : '');
+	address += buildingNumber + ' ';
+	address += street + ' ';
+	address += suburb + ' ';
+	address += state + ' ';
+	address += postcode + ' ';
+
+	return address;
+}
